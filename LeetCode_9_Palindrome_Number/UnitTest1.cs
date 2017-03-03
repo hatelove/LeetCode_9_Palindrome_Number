ebce2c25bdd525afb875c184f44bbbb92c98a206
hatelove <jay.hatelove@gmail.com>
2017-03-03 14:08:12 +0800

    add the first test case: x.length is 1, should return true;

---
+++ LeetCode_9_Palindrome_Number/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_9_Palindrome_Number
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void x_length_is_1_should_be_palindrome()
+        {
+            var x = 5;
+            Assert.IsTrue(new Solution().IsPalindrome(x));
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
