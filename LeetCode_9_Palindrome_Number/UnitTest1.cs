56eaeaff04a3703c1a95b7483d297b5b04e8eefd
hatelove <jay.hatelove@gmail.com>
2017-03-03 14:18:28 +0800

    pass test case: x is 55

--- LeetCode_9_Palindrome_Number/UnitTest1.cs
+++ LeetCode_9_Palindrome_Number/UnitTest1.cs
@@ -28,7 +28,23 @@ namespace LeetCode_9_Palindrome_Number
         public bool IsPalindrome(int x)
         {
             var charArray = x.ToString().ToCharArray();
-            return charArray.Length == 1;
+
+            if (charArray.Length == 1) return true;
+
+            var start = 0;
+            var end = charArray.Length - 1;
+            while (start < end)
+            {
+                if (charArray[start] != charArray[end])
+                {
+                    return false;
+                }
+
+                start++;
+                end--;
+            }
+
+            return true;
         }
     }
 }
